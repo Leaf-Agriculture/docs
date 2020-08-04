a9db6b245b59092bc686d09573f819a67d4c96f4
Joao Vicente Meyer <1994meyer@gmail.com>
2020-08-04 11:49:47 -0300

    Removed footer that was causing broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'Leaf Agriculture',
   tagline: 'The easiest way to connect agriculture data across platforms.',
   url: 'https://leaf-agriculture.github.io',
-  baseUrl: '/docs/',
+  baseUrl: '/',
   favicon: 'img/favicon.ico',
   organizationName: 'leaf-agriculture', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
@@ -36,56 +36,6 @@ module.exports = {
           position: 'right',
         },
       ],
-    },
-
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
 

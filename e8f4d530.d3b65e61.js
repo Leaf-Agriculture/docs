24abc4322639933f8f3c883ea75b56839786e7e1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2024-03-27 20:38:50 +0000

    Deploy Leaf-Agriculture/docs to github.com/Leaf-Agriculture/docs.git:gh-pages

---
+++ e8f4d530.d3b65e61.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[86],{140:function(o){o.exports=JSON.parse('{"allTagsPath":"/docs/blog/tags","slug":"rick-roll","name":"rick roll","count":1,"permalink":"/docs/blog/tags/rick-roll"}')}}]);

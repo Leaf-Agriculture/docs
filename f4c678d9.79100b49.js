1390d7dec5944357d5e6a67cd7c9b48f57728326
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2024-07-01 14:02:58 +0000

    Deploy Leaf-Agriculture/docs to github.com/Leaf-Agriculture/docs.git:gh-pages

---
+++ f4c678d9.79100b49.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[98],{152:function(o){o.exports=JSON.parse('{"permalink":"/docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog"}')}}]);
